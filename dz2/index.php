a1459b59691dd12428cfddc1e31d2b8ef121b8fa
Alen Varazdinac <alen.varazdinac@gmail.com>
2017-05-31 10:25:18 +0200

    Add files via upload

---
+++ dz2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    
+    <body>
+        <div id="circle" style="left:<?php echo $_GET["left"] ?>px; top:<?php echo $_GET["top"] ?>px;"></div>
+        
+        
+        <style>
+            #circle {
+                border-radius: 50%;
+                background-color: #7425b6;
+                width: 50px;
+                height: 50px;
+                position: absolute;
+            }
+        </style>
+    </body>
+    
+</html>
